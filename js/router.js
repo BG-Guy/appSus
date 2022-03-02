e6e7ffd7626685f58cc22e45936f722eef2fe096
BG-Guy <53979740+BG-Guy@users.noreply.github.com>
2022-03-02 11:02:37 +0200

    updated routed in router

--- js/router.js
+++ js/router.js
@@ -1,8 +1,6 @@
-import carApp from './views/car-app.cmp.js';
-import homePage from './views/home-page.cmp.js'
-import aboutPage, {aboutTeam, aboutServices} from './views/about-page.cmp.js'
-import carDetails from './views/car-details.cmp.js';
-import carEdit from './views/car-edit.cmp.js';
+import homePage from './pages/app-home.cmp.js'
+import aboutPage from './pages/app-about.cmp'
+
 
 const routes = [
     {
@@ -12,45 +10,31 @@ const routes = [
     {
         path: '/mail',
         component: aboutPage,
-        children: [
-            {
-                path: 'team',
-                component: aboutTeam
-            },
-            {
-                path: 'services',
-                component: aboutServices
-            },
-        ]
+        // children: [
+        //     {
+        //         path: 'team',
+        //         component: aboutTeam
+        //     },
+        //     {
+        //         path: 'services',
+        //         component: aboutServices
+        //     },
+        // ]
     },
 
     {
         path: '/keep',
         component: aboutPage,
-        children: [
-            {
-                path: 'team',
-                component: aboutTeam
-            },
-            {
-                path: 'services',
-                component: aboutServices
-            },
-        ]
-    },
-
-    
-    {
-        path: '/car',
-        component: carApp
-    },
-    {
-        path: '/car/:carId',
-        component: carDetails
-    },
-    {
-        path: '/car/edit/:carId?',
-        component: carEdit
+        // children: [
+        //     {
+        //         path: 'team',
+        //         component: aboutTeam
+        //     },
+        //     {
+        //         path: 'services',
+        //         component: aboutServices
+        //     },
+        // ]
     },
 ];
 
