e5b1dc28d01301426853e7b269cef16099308544
BG-Guy <53979740+BG-Guy@users.noreply.github.com>
2022-03-02 12:34:18 +0200

    route updated

--- js/router.js
+++ js/router.js
@@ -1,11 +1,7 @@
 import homePage from './pages/app-home.cmp.js'
-<<<<<<< HEAD
 import appKeep from './keep/pages/keep-app.cmp.js'
 import appMail from './mail/pages/mail-app.cmp.js'
-=======
-import aboutPage from './pages/app-about.cmp'
-import aboutPag from './pages/app-about.cmp'
->>>>>>> b7a190a10a119cda26f60344877de8fdb5e2264c
+
 
 const routes = [
     {
